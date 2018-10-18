a0f3298f709baa4baa377ae3ce24ff2355079b90
Thigo Medeiros <si-thiago@hotmail.com>
2018-10-18 17:56:30 -0300

    3) Adicione um botão de nome "Nova Tarefa" e um campo de <input>    para adicionar uma tarefa a lista

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 
 class App extends Component {
   state = {
-    tarefas: ['Tarefa 1', 'Tarefa 2']
+    tarefas: ['Tarefa 1', 'Tarefa 2'],
+    novaTarefa: ''
+  }
+
+  adicionarTarefa() {
+    let tarefasAntigas = this.state.tarefas
+    this.setState({
+      tarefas: tarefasAntigas.concat(this.state.novaTarefa)
+    })
+  }
+
+  atualizaNovaTarefa(event) {
+    this.setState({
+      novaTarefa: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <p>Tarefas</p>
+        <input type="text" value={this.state.novaTarefa}
+          onChange={this.atualizaNovaTarefa.bind(this)}/>
+        <button onClick={this.adicionarTarefa.bind(this)}>Nova Tarefa</button>
         <ul>
           {this.state.tarefas.map(
-            (tarefa) => {
-              return <li>{tarefa}</li>
+            (tarefa, index) => {
+              return <li key={index}>{tarefa}</li>
             }
           )}
         </ul>
