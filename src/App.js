e16142ac1c4b468f7d42066d95162662ac4f57e0
Thigo Medeiros <si-thiago@hotmail.com>
2018-10-18 17:46:23 -0300

    2) Imprima um array de tarefas nesta <div> usando state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    tarefas: ['Tarefa 1', 'Tarefa 2']
+  }
+
   render() {
     return (
       <div>
+        <p>Tarefas</p>
+        <ul>
+          {this.state.tarefas.map(
+            (tarefa) => {
+              return <li>{tarefa}</li>
+            }
+          )}
+        </ul>
       </div>
     );
   }
